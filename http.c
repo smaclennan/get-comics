d00e58e5e58d49385eec2ec202c01118a3386e20
Sean MacLennan <git@seanm.ca>
2011-12-10 00:38:29 -0500

    Grrr....

--- http.c
+++ http.c
@@ -197,18 +197,6 @@ static void add_full_header(struct connection *conn)
 
 	n += snprintf(conn->buf + n, BUFSIZE - n,
 		      "Connection: keep-alive\r\n");
-	n += snprintf(conn->buf, BUFSIZE - n,
-		      "User-Agent: Mozilla/5.0 (X11; Linux i686; rv:6.0.2) "
-		      "Gecko/20100101 Firefox/6.0.2\r\n");
-	n += snprintf(conn->buf, BUFSIZE - n,
-		      "Accept: text/html,application/xhtml+xml,application/"
-		      "xml;q=0.9,*/*;q=0.8\r\n");
-	n += snprintf(conn->buf, BUFSIZE - n,
-		      "Accept-Language: en-us,en;q=0.5\r\n");
-	n += snprintf(conn->buf, BUFSIZE - n,
-		      "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7\r\n");
-	n += snprintf(conn->buf, BUFSIZE - n,
-		      "Connection: keep-alive\r\n");
 #endif
 }
 
