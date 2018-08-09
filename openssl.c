0fdaabb8b6d72362bdfcca89d25d4c5dc7cee9e5
Sean MacLennan <seanm@seanm.ca>
2018-08-09 13:21:37 -0400

    Neither of these calls are needed any more, and SSLeay_add_all_algorithms() no longer exists in openssl 1.1.

--- openssl.c
+++ openssl.c
@@ -28,8 +28,6 @@ static int openssl_init(void)
 
 	SSL_library_init();
 	SSL_load_error_strings();
-	SSLeay_add_all_algorithms();
-	SSLeay_add_ssl_algorithms();
 
 	ssl_ctx = SSL_CTX_new(SSLv23_client_method());
 	if (!ssl_ctx) {
